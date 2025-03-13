c83f163f9f2d27fb5bad08b0680f364ddf2e8e50
Dillion Huston <122535848+dillionhuston@users.noreply.github.com>
2025-03-13 14:27:50 +0000

    save data to json

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
+import html_to_json
 
-url = "https://www.onefamily.com/help/fund-information/daily-prices/" 
-USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101"
-headers = {"user-agent": USER_AGENT}
-resp = requests.get(url, headers=headers)
-BeautifulSoup(resp.content, "html.parser" )
-print(resp.text)
+stock_data = 'resp.json'
+
+
+def Scrape():
+    url = "https://www.onefamily.com/help/fund-information/daily-prices/" 
+    USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101"
+    headers = {"user-agent": USER_AGENT}
+    resp = requests.get(url, headers=headers)
+   
+    if resp.status_code == 200:
+        page_soup = BeautifulSoup(resp.content, "html.parser")
+        return page_soup 
+    else:
+        print(f"failed status code: {resp.status_code}")
+        return None  
+
+
+def StoreData():
+    page_soup = Scrape()  
+    
+    if page_soup:
+      
+        data_output = html_to_json.convert(str(page_soup))  
+        print(data_output)
+      
+        with open(stock_data, 'w') as json_file:
+            json_file.write(str(data_output)) 
+        print("ni data.")
+
+Scrape()  
+StoreData()  
